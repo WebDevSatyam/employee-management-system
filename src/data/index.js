da0cfbd2e539bd64884793935a4f24b05831d364
WebDevSatyam <samk5092@gmail.com>
2023-08-23 16:38:39 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,52 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Joe',
+    lastName: 'Tribbiani',
+    email: 'joe@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Chandler',
+    lastName: 'Bing',
+    email: 'chandler@example.com',
+    salary: '120000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Ross',
+    lastName: 'Geller',
+    email: 'ross@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Rachel',
+    lastName: 'Green',
+    email: 'rachel@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Monika',
+    lastName: 'Geller',
+    email: 'monika@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Phoebe',
+    lastName: 'Buffay',
+    email: 'phoebe@example.com',
+    salary: '80000',
+    date: '2019-07-30'
+  },
+];
+
+export { employeesData };
